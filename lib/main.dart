a0b15c70add68bb0e565ac0d026552050eae4686
이소윤 <s_lee77176@fanshaweonline.ca>
2021-10-28 19:23:57 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
+    return CupertinoApp(
       home: const MyHomePage(),
     );
   }
@@ -28,133 +25,56 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late String _asset;
-  late double _lampHeight;
-  late double _lampWidth;
-  late bool _lampSizeStatus;
-  late bool _lampTurn;
-  late bool _lampRed;
+  // late String _asset;
+  // late double _lampHeight;
+  // late double _lampWidth;
+  // late bool _lampSizeStatus;
+  // late bool _lampTurn;
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
+  // @override
+  // void initState() {
+  //   // TODO: implement initState
+  //   super.initState();
 
-    _asset = "images/lamp_on.png";
-    _lampHeight = 300;
-    _lampWidth = 150;
-    _lampSizeStatus = false;
-    _lampTurn = true;
-    _lampRed = false;
-  }
+  //   _asset = "images/lamp_on.png";
+  //   _lampHeight = 300;
+  //   _lampWidth = 150;
+  //   _lampSizeStatus = false;
+  //   _lampTurn = true;
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Light Bulb Switch"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    _asset,
-                    width: _lampWidth,
-                    height: _lampHeight,
-                  ),
-                ],
-              ),
-              width: 350,
-              height: 650,
-            ),
-            Row(
+    return CupertinoPageScaffold(
+      child: Column(
+        children: [
+          SizedBox(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Column(
-                  children: [
-                    const Text("Size"),
-                    Switch(
-                        value: _lampSizeStatus,
-                        onChanged: (value) {
-                          setState(() {
-                            _lampSizeStatus = value;
-                            sizeFunc();
-                          });
-                        }),
-                  ],
-                ),
-                Column(
-                  children: [
-                    const Text("Switch"),
-                    Switch(
-                        value: _lampTurn,
-                        onChanged: (value) {
-                          setState(() {
-                            _lampTurn = value;
-                            turnFunc();
-                          });
-                        }),
-                  ],
-                ),
-                Column(
-                  children: [
-                    const Text("Colour"),
-                    Switch(
-                        value: _lampRed,
-                        onChanged: (value) {
-                          setState(() {
-                            _lampRed = value;
-                            turnRed();
-                          });
-                        })
-                  ],
-                )
+                Image.asset("images/lamp_off.png"),
               ],
             ),
-          ],
-        ),
+            width: 350,
+            height: 650,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  const Text("Size"),
+                ],
+              ),
+              Column(
+                children: [
+                  const Text("Switch"),
+                ],
+              )
+            ],
+          ),
+        ],
       ),
     );
-  }
-
-  sizeFunc() {
-    if (_lampSizeStatus == true) {
-      _lampWidth = 300;
-      _lampHeight = 600;
-    } else {
-      _lampWidth = 150;
-      _lampHeight = 300;
-    }
-  }
-
-  turnFunc() {
-    if (_lampTurn == true) {
-      _asset = "images/lamp_on.png";
-    } else {
-      _asset = "images/lamp_off.png";
-    }
-  }
-
-  turnRed() {
-    if (_lampRed == false && _lampTurn == false) {
-      _asset = "images/lamp_off.png";
-    }
-
-    if (_lampRed == true && _lampTurn == true) {
-      _asset = "images/lamp_red.png";
-    }
-
-    if (_lampTurn == true && _lampRed == false) {
-      _asset = "images/lamp_on.png";
-    }
-
-    if (_lampRed == true && _lampTurn == false) {
-      _asset = "images/lamp_off.png";
-    }
   }
 }
